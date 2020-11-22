e37e056cb5e34d10ecf468401e5ec4df97b7e2ab
Rahul Rajeev <30558316+rahulrajeev21@users.noreply.github.com>
2020-11-22 04:01:25 -0800

    Added real data, moved legend to bottom

--- donutChart.js
+++ donutChart.js
@@ -23,24 +23,43 @@ function change(data) {
 
 Promise.all([
     d3.csv("dnf_all.csv"),
+    d3.csv("dnf_caterham.csv"),
+    d3.csv("dnf_ferrari.csv"),
+    d3.csv("dnf_forceIndia.csv"),
+    d3.csv("dnf_hrt.csv"),
+    d3.csv("dnf_lotus.csv"),
+    d3.csv("dnf_marussia.csv"),
     d3.csv("dnf_merc.csv"),
-    d3.csv("dnf_toroRosso.csv")
+    d3.csv("dnf_rbr.csv"),
+    d3.csv("dnf_renault.csv"),
+    d3.csv("dnf_sauber.csv"),
+    d3.csv("dnf_spyker.csv"),
+    d3.csv("dnf_toroRosso.csv"),
+    d3.csv("dnf_virgin.csv"),
+    d3.csv("dnf_williams.csv")
 ]).then(function(files) {
-    // files[0] will contain file1.csv
     allConstructors = files[0];
-    // files[1] will contain file2.csv
-    mercedes = files[1];
-    toroRosso = files[2];
+    caterham = files[1];
+    ferrari = files[2];
+    forceIndia = files[3];
+    hrt = files[4];
+    lotus = files[5];
+    marussia = files[6];
+    mercedes = files[7];
+    rbr = files[8];
+    renault = files[9];
+    sauber = files[10];
+    spyker = files[11];
+    toroRosso = files[12];
+    virgin = files[13];
+    williams = files[14];
     var width = 360;
     var height = 360;
     var radius = Math.min(width, height) / 2;
     var donutWidth = 75; //This is the size of the hole in the middle
 
-//Only choose one! This one for a d3 color scheme:
-    var color = d3.scaleOrdinal(d3.schemeSet1);
-//Or this one for a customized color scheme:
-// var color = d3.scaleOrdinal()
-//     .range(["#5A39AC", "#DD98D6", "#E7C820", "#08B2B2"]);
+var color = d3.scaleOrdinal()
+    .range(["#1b70fc", "#faff16", "#d50527", "#158940", "#f898fd", "#24c9d7", "#cb9b64", "#866888", "#22e67a", "#e509ae", "#9dabfa", "#437e8a", "#b21bff", "#ff7b91", "#94aa05", "#ac5906", "#82a68d", "#fe6616", "#7a7352", "#f9bc0f", "#b65d66", "#07a2e6", "#c091ae", "#8a91a7", "#88fc07", "#ea42fe", "#9e8010", "#10b437","#1b70fc", "#faff16", "#d50527", "#158940", "#f898fd", "#24c9d7", "#cb9b64", "#866888", "#22e67a", "#e509ae", "#9dabfa", "#437e8a", "#b21bff", "#ff7b91", "#94aa05", "#ac5906", "#82a68d", "#fe6616", "#7a7352", "#f9bc0f", "#b65d66", "#07a2e6", "#c091ae", "#8a91a7", "#88fc07", "#ea42fe", "#9e8010", "#10b437"]);
 
     var svg = d3.select('#donut')
         .append('svg')
@@ -92,33 +111,31 @@ Promise.all([
                 .style("opacity", 0);
         });
 
-    var legendRectSize = 13;
-    var legendSpacing = 7;
-    var legend = svg.selectAll('.legend') //the legend and placement
+    // select legend
+    var legend = d3.select("#legend")
+
+// Add one dot in the legend for each name.
+    legend.selectAll("mydots")
         .data(color.domain())
         .enter()
-        .append('g')
-        .attr('class', 'circle-legend')
-        .attr('transform', function (d, i) {
-            var height = legendRectSize + legendSpacing;
-            var offset = height * color.domain().length / 2;
-            var horz = -2 * legendRectSize - 13;
-            var vert = i * height - offset;
-            return 'translate(' + horz + ',' + vert + ')';
-        });
-    legend.append('circle') //keys
-        .style('fill', color)
-        .style('stroke', color)
-        .attr('cx', 0)
-        .attr('cy', 0)
-        .attr('r', '.5rem');
-    legend.append('text') //labels
-        .attr('x', legendRectSize + legendSpacing)
-        .attr('y', legendRectSize - legendSpacing)
-        .attr('fill','white')
-        .text(function (d) {
-            return d;
-        });
+        .append("circle")
+        .attr("cx", 100)
+        .attr("cy", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("r", 7)
+        .style("fill", function(d){ return color(d)})
+
+// Add one dot in the legend for each name.
+    legend.selectAll("mylabels")
+        .data(color.domain())
+        .enter()
+        .append("text")
+        .attr("x", 120)
+        .attr("y", function(d,i){ return 100 + i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function(d){ return color(d)})
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
+
     d3.select("select#constructorList")
         .on("change",function(d){
             var selected = d3.select("#constructorList").node().value;
