6660b3f614c35155cff151f9e5483a1137ac0797
Rahul Rajeev <30558316+rahulrajeev21@users.noreply.github.com>
2020-11-27 23:04:06 -0800

    vega experiments

---
+++ concatviz.js
@@ -0,0 +1,32 @@
+var accidentRatingBar = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "data": {"url": "pitstops.json"},
+    "width": 400,
+    "height": 200,
+    "vconcat": [{
+        "selection": {
+            "brush": {"type": "interval"}
+        },
+        "mark": "point",
+        "encoding": {
+            "x": {
+                "field": "Laps", "type": "quantitative",
+                "scale": {"domain": [10,70]}
+            },
+            "y": {
+                "field": "PitstopMs", "type": "quantitative"
+                // "scale": {"domain": [0, 500]}
+            }
+        }
+    }, {
+        "transform": [
+            {"filter": {"selection": "brush"}}
+        ],
+        "mark": "line",
+        "encoding": {
+            "x": {"field": "Points", "type": "quantitative"},
+            "y": {"field": "Position", "type": "quantitative","sort":"descending"}
+        }
+    }]
+}
+vegaEmbed('#concatviz', accidentRatingBar);
